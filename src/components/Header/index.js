30bf5befdbf31ea57379657c31044cea0810ee56
Dany Grimaldo <danygrimaldo@gmail.com>
2021-07-01 13:59:42 -0400

    removed warnings issues

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image } from "react-bootstrap";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import gmLogo from "./Assets/grimaldomedia-rgb-logo.png";
 import { Home } from "../../pages/Home";
 import "./style.css";
